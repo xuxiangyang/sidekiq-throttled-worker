deddd97b6af33abdcb9409007be51036c3be45c8
xuxiangyang <xxy@xuxiangyang.com>
2020-10-27 21:31:22 +0800

    rm check limit

--- lib/sidekiq/throttled_worker/concurrency.rb
+++ lib/sidekiq/throttled_worker/concurrency.rb
@@ -6,20 +6,14 @@ module Sidekiq
           # only allow one worker check limit
           concurrency = worker_class.get_sidekiq_options["concurrency"]
           ttl = (worker_class.get_sidekiq_options["concurrency_ttl"] || 900) # assume worker should finish run in 15 minutes, and worker may be block completely max for 15 minutes in extreme case
-          check_limit_key = "sidekiq:throttled_worker:check_limit:#{worker_class}"
           now = Time.now.to_f
           Sidekiq.redis do |conn|
-            return true unless conn.set(check_limit_key, 1, nx: true, ex: 60)
             conn.zremrangebyscore(concurrency_key(worker_class), "-inf", "(#{now}")
             return true if conn.zcard(concurrency_key(worker_class)) >= concurrency && conn.zscore(concurrency_key(worker_class), jid).nil?
             conn.zadd(concurrency_key(worker_class), (now + ttl).to_i, jid)
             conn.expire(concurrency_key(worker_class), (now + ttl).to_i)
 
             false
-          end
-        ensure
-          Sidekiq.redis do |conn|
-            conn.del(check_limit_key)
           end
         end
 
